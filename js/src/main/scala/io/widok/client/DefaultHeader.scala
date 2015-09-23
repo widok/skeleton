a9132a3a4be4a20eb5cf7540dac3d987ddbdec94
Marius B. Kotsbak <marius.kotsbak@webstep.no>
2015-09-23 15:17:47 +0200

    Upgrade to Widok 0.2.3 and matching versions of Scala and Scala.Js

--- js/src/main/scala/io/widok/client/DefaultHeader.scala
+++ js/src/main/scala/io/widok/client/DefaultHeader.scala
@@ -2,14 +2,19 @@ package io.widok.client
 
 import org.widok._
 import org.widok.bindings.Bootstrap._
+import pl.metastack.metarx.Var
 
 trait DefaultHeader {
+  val stateClosed = Var(true)
+
   def header() =
     NavigationBar(
       Container(
         NavigationBar.Header(
-          NavigationBar.Toggle(),
-          NavigationBar.Brand(Globals.brandName)
+          NavigationBar.Toggle(stateClosed),
+          NavigationBar.Collapse(stateClosed)(
+            NavigationBar.Brand(Globals.brandName)
+          )
         )
       )
     )
